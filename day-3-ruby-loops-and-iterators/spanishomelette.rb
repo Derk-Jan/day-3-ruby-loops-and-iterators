6f940d8b58445543fbffdc59b5e3a206a1e83d05
derk-jan <derk.vandervaart@gmail.com>
2017-03-07 15:35:51 +0100

    Define the Ingredients

---
+++ day-3-ruby-loops-and-iterators/spanishomelette.rb
@@ -0,0 +1,23 @@
+def print_divider
+  puts "*" * 40
+  puts "\n"
+end
+
+def print_progress_bar
+  3.times { sleep 0.5; print "." }
+  puts "\n"
+end
+
+POUNDS_FLOUR = 4
+NUM_EGGS = 24
+# TEASPOONS_CINNAMON = 2
+LITRES_MILK = 12
+
+#1. Defining the Ingredients
+
+ingredients = [
+  { name: 'flour', quantity: POUNDS_FLOUR },
+  { name: 'eggs', quantity: NUM_EGGS },
+  { name: 'cinnamon', quantity: 'some' },
+  { name: 'milk', quantity: 'LITRES_MILK }
+]
