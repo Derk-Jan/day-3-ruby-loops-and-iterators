f40436afbeb368af80daffad794f5ebe57fd8c37
derk-jan <derk.vandervaart@gmail.com>
2017-03-07 16:04:24 +0100

    define recipe steps

--- day-3-ruby-loops-and-iterators/spanishomelette.rb
+++ day-3-ruby-loops-and-iterators/spanishomelette.rb
@@ -33,3 +33,40 @@ puts "You need the following ingredients:"
 ingredients.each do |ingredient|
   puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
 end
+
+# 3. Asking user if he got all ingredients
+
+loop do
+  puts "\n"
+  print_divider
+
+  puts "Got all ingredients you need? (Y/N)"
+  answer = gets.chomp.upcase
+
+  if answer == 'N'
+    puts "Bummer. You need to go to the Albert-Hein"
+    print_progress_bar
+  elsif answer == 'Y'
+    puts "Ready, steady, cook!"
+    break
+  else
+    puts "That's not a valid input. Try again."
+  end
+end
+
+# 4. Defining the Recipe Steps
+
+steps = [
+  { description: "get an huge stirring bowl.I mean HUGE!", action: "get_an_stirring_bowl" },
+  { description: "put the flour in the stirring bowl", action: "flour-in_stirring bowl" },
+  { description: "pour the milk in the stirring bowl", action: "pour_milk" },
+  { description: "Stir into the bowl the flour with milk and some salt", action: "generic_recipe_step" },
+  { description: "Break the eggs", action: "break_eggs" },
+  { description: "pour the eggs in the stirring bowl", action: "generic_recipe_step" },
+  { description: "Stir the eggs together with the other ingredients", action: "generic_recipe_step" },
+  { description: "Break the eggs", action: "break_eggs" },
+  { description: "Heat the oil in a large frying pan", action: "generic_recipe_step"},
+  { description: "Cook until the pancake is solid and slightly brown", action: "generic_recipe_step" }
+  { description: "flip the pancake in the air and catch it in the pan" action: "magic-recipe_step"}
+  {description: "Cook until the bottom side is lightly brown", action: "generic_recipe_step"}
+]
